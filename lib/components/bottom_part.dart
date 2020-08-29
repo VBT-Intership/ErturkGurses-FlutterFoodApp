d060ef38ddf73543953fd78c8e546fabfdb294f6
erturkgurses <erturkgurses@hotmail.com>
2020-08-30 02:12:16 +0300

    Created bottom part of home page

---
+++ lib/components/bottom_part.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:vbt_practice/components/vertical_listView.dart';
+
+class BottomPart extends StatelessWidget {
+  const BottomPart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VerticalListView();
+  }
+}
