b492cee746ca8a419a71e272a195c55587ca01f6
erturkgurses <erturkgurses@hotmail.com>
2020-08-30 02:17:19 +0300

    Created vertical listView to show popular images

---
+++ lib/components/vertical_listView.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:vbt_practice/components/popular_card.dart';
+
+class VerticalListView extends StatelessWidget {
+  const VerticalListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: EdgeInsets.only(left: 20),
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PopularCard(index:index)
+          );
+        });
+  }
+}
