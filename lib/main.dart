21c86ed30c3e280adaff944faea1d5b61364f2f5
erturkgurses <erturkgurses@hotmail.com>
2020-08-30 02:12:00 +0300

    Added action button and changed leading button

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
- 
+import 'package:vbt_practice/views/home.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'), 
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leading: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(Icons.short_text, color: Colors.black),
+              ),
+            ],
           ),
-        ),
-      ),
+          body: HomePage()),
     );
   }
 }
