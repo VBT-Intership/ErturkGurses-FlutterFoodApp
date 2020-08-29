93711c8c58d7e3faa0cfb96e70df174d5e4367a9
erturkgurses <erturkgurses@hotmail.com>
2020-08-30 02:15:03 +0300

    Created middle part of page.

---
+++ lib/components/middle_part.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MiddlePart extends StatelessWidget {
+  const MiddlePart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Row(children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: Card(
+              color: Colors.red,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.favorite, color: Colors.white),
+              )),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5.0),
+              child: Text('Popular',
+                  style: TextStyle(
+                      height: 1.5, fontSize: 17, fontWeight: FontWeight.bold)),
+            ),
+            Text('Monggo,entekno duwekmu!',
+                style: TextStyle(fontWeight: FontWeight.w300)),
+          ],
+        ),
+      ]),
+      Icon(Icons.arrow_forward)
+    ]);
+  }
+}
